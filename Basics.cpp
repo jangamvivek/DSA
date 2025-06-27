3aff7f90772451c6dc4bf3f0cc4725996639e283
vivek jangam <vivekjangam19@gmail.com>
2025-06-27 07:23:42 +0530

    Lower bound using linear search and Binary search

--- Basics.cpp
+++ Basics.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <set>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 
@@ -39,51 +40,101 @@ using namespace std;
 
 // USING MAP 
 
-int main(){
-  int n;
-  cout << "Enter no of elements: ";
-  cin >> n;
+// int main(){
+//   int n;
+//   cout << "Enter no of elements: ";
+//   cin >> n;
 
-  int arr[n];
-  cout << "Enter array elements: " << endl;
+//   int arr[n];
+//   cout << "Enter array elements: " << endl;
 
-  // pre-order for map
-  // map<int, int> mpp;
+//   // pre-order for map
+//   // map<int, int> mpp;
 
-  // suppose if i use unordered_map then it will give unordered elements 
-  // Frequencies:
-  // 4 -> 1
-  // 12 -> 1
-  // 2 -> 2
-  // 1 -> 1 like this
+//   // suppose if i use unordered_map then it will give unordered elements 
+//   // Frequencies:
+//   // 4 -> 1
+//   // 12 -> 1
+//   // 2 -> 2
+//   // 1 -> 1 like this
 
-  // pre-order for unordered map
-  unordered_map<int, int> mpp;
+//   // pre-order for unordered map
+//   unordered_map<int, int> mpp;
 
-  for (int i = 0; i < n; i++)
-  {
-    cin >> arr[i];
-    mpp[arr[i]]++;
-  }
+//   for (int i = 0; i < n; i++)
+//   {
+//     cin >> arr[i];
+//     mpp[arr[i]]++;
+//   }
   
-  // iterate in the map 
-  cout << "Frequencies:\n";
-    unordered_map<int, int>::iterator it;
-    for (it = mpp.begin(); it != mpp.end(); ++it) {
-        cout << it->first << " -> " << it->second << endl;
-    }
+//   // iterate in the map 
+//   cout << "Frequencies:\n";
+//     unordered_map<int, int>::iterator it;
+//     for (it = mpp.begin(); it != mpp.end(); ++it) {
+//         cout << it->first << " -> " << it->second << endl;
+//     }
   
 
-  int q;
-    cout << "Enter number of queries: ";
-    cin >> q;
+//   int q;
+//     cout << "Enter number of queries: ";
+//     cin >> q;
 
-    while (q--) {
-        int number;
-        cout << "Enter number to find frequency: ";
-        cin >> number;
-        cout << "Frequency of " << number << " is: " << mpp[number] << endl;
+//     while (q--) {
+//         int number;
+//         cout << "Enter number to find frequency: ";
+//         cin >> number;
+//         cout << "Frequency of " << number << " is: " << mpp[number] << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// LOWER BOUND IN AN ARRAY USING LINEAR SEARCH 
+// int lowerBound(vector<int> &arr, int target) {
+//     int n = arr.size();
+	
+//     for (int i = 0; i < n; ++i) {
+
+//       	if(arr[i] >= target) {
+//           	return i;
+//         }
+//     }
+//   	return n;
+// }
+
+// int main() {
+//     vector<int> arr = {2, 3, 7, 10, 11, 11, 25};
+//     int target = 9;
+//     int lb;
+
+//     cout << lowerBound(arr, target);
+//     return 0;
+// }
+
+
+// USING BINARY SEARCH 
+int lowerBound(vector<int> &arr, int target){
+    int lo = 0,   hi = arr.size() - 1;
+    int res = arr.size();
+
+    while (lo <= hi)
+    {
+      int mid = lo + (hi - lo) / 2;
+        if(arr[mid] >= target){
+          res = mid;
+          hi = mid - 1;
+        }
+        else{
+          lo = mid + 1;
+        }
     }
+    return res;
+}
+int main() {
+    vector<int> arr = {2, 3, 7, 10, 11, 11, 25};
+    int target = 26;
 
+    cout << lowerBound(arr, target);
     return 0;
 }
