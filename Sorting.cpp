b99f91d391282a7cf6a9c46eff2e9d3a836b134f
vivek jangam <vivekjangam19@gmail.com>
2025-07-06 21:43:19 +0530

    Bubble Sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // int selection_sort(int arr[], int n){
 //   for(int i=0; i <= n-2; i++){
 //     int mini = i;
-//     for(int j=1; j <= n - 1; j++){
+//     for(int j = i + 1; j < n; j++){
 //        if(arr[j] < arr[mini]){
 //         mini = j;
 //        }
@@ -38,12 +38,17 @@ using namespace std;
 // BUBBLE SORT 
 void bubble_sort(int arr[], int n){
   for(int i = n - 1; i >=0; i--){
+    int didSwap = 0;
     for(int j = 0; j <= i - 1; j++){
        if(arr[j] > arr[j + 1]){
          int temp = arr[j + 1];
          arr[j+1] = arr[j];
          arr[j] = temp;
+         didSwap = 1;
        }
+    }
+    if (didSwap == 0){
+      break;
     }
   }
 }
