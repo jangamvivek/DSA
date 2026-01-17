21002f23467070d14a3ae4679addbe98f07bc59c
vivek jangam <152076907+jangamvivek@users.noreply.github.com>
2026-01-17 14:18:23 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //last index nums1
-        int last = m + n - 1;
-
-        //merge in reverse order
-        while(m>0 && n>0){
-            if(nums1[m-1] > nums2[n-1]){
-                nums1[last] = nums1[m - 1];
-                m--;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j>=0){
+            if(nums1[i] >= nums2[j]){
+                nums1[k--] = nums1[i--];
             }else{
-                nums1[last] = nums2[n - 1];
-                n--;
+                nums1[k--] = nums2[j--];
             }
-            last--;
         }
-        //fill nums1 with leftover nums2 elements 
-        while (n > 0) {
-            nums1[last] = nums2[n - 1];
-            n--;
-            last--;
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
         }
-
     }
 };
