14f37b51dde1b20d6972ea2955dcc00e0145250f
vivek jangam <152076907+jangamvivek@users.noreply.github.com>
2026-03-04 20:25:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (19.29%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> freq(26,0);
+        vector<bool> visited(26,false);
+        stack<char> st;
+
+        for(char c : s){
+            freq[c-'a']++;
+        }
+
+        for(char c : s){
+
+            freq[c-'a']--;
+
+            if(visited[c-'a'])
+                continue;
+
+            while(!st.empty() && st.top() > c && freq[st.top()-'a'] > 0){
+                visited[st.top()-'a'] = false;
+                st.pop();
+            }
+
+            st.push(c);
+            visited[c-'a'] = true;
+        }
+
+        string res = "";
+
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
