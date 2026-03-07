45d1ef0b72c565b2d90b680e9a85e14b8b2f0a51
vivek jangam <152076907+jangamvivek@users.noreply.github.com>
2026-03-07 11:55:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.35%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int odd = 1;
+        while(num > 0){
+            num -= odd;
+            odd += 2;
+        }
+        return num == 0;
+    }
+};
